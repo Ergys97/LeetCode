8809a5aa0d2cfe8c9ff3084d55abec337e3790fc
Ergys Perdeda <ergysperdeda97@gmail.com>
2024-03-24 10:56:04 +0100

    Time: 70 ms (51.02%) | Memory: 50.7 MB (46.92%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let numIndices = {};
+    
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        
+        if (numIndices.hasOwnProperty(complement)) {
+            return [numIndices[complement], i];
+        }
+        
+        numIndices[nums[i]] = i;
+    }
+    
+    return [];
+};
