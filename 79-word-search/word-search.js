345f42ab3da19ab50d3aeae72544e85c3f7f3c14
Ergys Perdeda <ergysperdeda97@gmail.com>
2024-04-03 15:09:00 +0200

    Time: 341 ms (86.45%) | Memory: 48.9 MB (87.31%) - LeetSync

---
+++ 79-word-search/word-search.js
@@ -0,0 +1,32 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function (board, word) {
+    const rows = board.length;
+    const cols = board[0].length;
+
+    function dfs(r, c, index) {
+        if (index === word.length) return true;
+        if (r < 0 || c < 0 || r === rows || c === cols || board[r][c] !== word[index]) return false;
+
+        let temp = board[r][c];
+        board[r][c] = '/';
+
+        if (dfs(r - 1, c, index + 1) || dfs(r + 1, c, index + 1) || dfs(r, c - 1, index + 1) || dfs(r, c + 1, index + 1)) {
+            return true;
+        }
+
+        board[r][c] = temp;
+        return false;
+    }
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (dfs(i, j, 0)) return true;
+        }
+    }
+
+    return false;
+};
