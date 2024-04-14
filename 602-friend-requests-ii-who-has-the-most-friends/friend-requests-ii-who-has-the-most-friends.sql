703eb853ab20176a5dcc02c7994dffa59acecf8b
Ergys Perdeda <ergysperdeda97@gmail.com>
2024-04-14 23:27:33 +0200

    Time: 678 ms (36.48%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,26 +1,11 @@
-SELECT id, num
+SELECT id, COUNT(*) AS num
 FROM (
-    SELECT id, COUNT(*) AS num
-    FROM (
-        SELECT requester_id AS id
-        FROM RequestAccepted
-        UNION ALL
-        SELECT accepter_id AS id
-        FROM RequestAccepted
-    ) AS Friends
-    GROUP BY id
-) AS Counts
-WHERE num = (
-    SELECT MAX(num)
-    FROM (
-        SELECT COUNT(*) AS num
-        FROM (
-            SELECT requester_id AS id
-            FROM RequestAccepted
-            UNION ALL
-            SELECT accepter_id AS id
-            FROM RequestAccepted
-        ) AS Friends
-        GROUP BY id
-    ) AS Counts
-);
+    SELECT requester_id AS id
+    FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id
+    FROM RequestAccepted
+) AS Friends
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
