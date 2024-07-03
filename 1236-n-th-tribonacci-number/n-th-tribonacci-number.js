307b7bf80421fcaea10ca58125984f39a23e350a
Ergys Perdeda <ergysperdeda97@gmail.com>
2024-07-03 11:56:39 +0200

    Time: 48 ms (72.87%) | Memory: 48.4 MB (83.55%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.js
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.js
@@ -2,12 +2,15 @@
  * @param {number} n
  * @return {number}
  */
-function tribonacci(n) {
-    if (n === 0) return 0;
-    if (n <= 2) return 1;
-    let dp = [0, 1, 1];
-    for (let i = 3; i <= n; i++) {
+var tribonacci = function(n) {
+    if(n === 0) return 0;
+    if(n === 1 || n === 2) return 1;
+    let dp = new Array(n+1);
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 1;
+    for(let i = 3; i <= n; i++){
         dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
     }
     return dp[n];
-}
+};
