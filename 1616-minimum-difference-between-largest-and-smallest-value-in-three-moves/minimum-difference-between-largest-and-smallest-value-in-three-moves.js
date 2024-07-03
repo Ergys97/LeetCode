5885da9ae31f135c863ab0670531fe1ddb198b6a
Ergys Perdeda <ergysperdeda97@gmail.com>
2024-07-03 10:20:10 +0200

    Time: 125 ms (25.00%) | Memory: 58.2 MB (52.08%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.js
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.js
@@ -3,10 +3,14 @@
  * @return {number}
  */
 var minDifference = function(nums) {
-    if (nums.length <= 4) {
-        return 0;
-    } else {
-        nums.sort((a, b) => a - b);
-        return Math.min(nums[nums.length - 1] - nums[3], nums[nums.length - 2] - nums[2], nums[nums.length - 3] - nums[1], nums[nums.length - 4] - nums[0]);
+    if(nums.length <= 4) return 0;
+
+    nums.sort((a,b) => a-b);
+    console.log(nums)
+    let n = nums.length;
+    let min = Number.MAX_SAFE_INTEGER;
+    for(let i=0; i<4; i++){
+        min = Math.min(min, nums[n-4+i] - nums[i]);
     }
+    return min;
 };
