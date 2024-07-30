7dc6d0b0678633a70c1989a42aeafe8610f24488
Ergys Perdeda <ergysperdeda97@gmail.com>
2024-07-30 15:43:16 +0200

    Time: 112 ms (45.57%) | Memory: 58.1 MB (37.97%) - LeetSync

---
+++ 661-image-smoother/image-smoother.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[][]} img
+ * @return {number[][]}
+ */
+var imageSmoother = function(img) {
+    const m = img.length;
+    const n = img[0].length;
+    const result = Array.from({ length: m }, () => Array(n).fill(0));
+
+    const directions = [
+        [0, 0], [0, 1], [0, -1],
+        [1, 0], [1, 1], [1, -1],
+        [-1, 0], [-1, 1], [-1, -1]
+    ];
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            let sum = 0;
+            let count = 0;
+
+            for (const [dx, dy] of directions) {
+                const x = i + dx;
+                const y = j + dy;
+
+                if (x >= 0 && x < m && y >= 0 && y < n) {
+                    sum += img[x][y];
+                    count++;
+                }
+            }
+
+            result[i][j] = Math.floor(sum / count);
+        }
+    }
+
+    return result;
+};
