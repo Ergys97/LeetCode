b7dd0e3ab30dac7de6f09febe74b5cd40160fdcc
Ergys Perdeda <ergysperdeda97@gmail.com>
2024-08-21 17:15:19 +0200

    Time: 77 ms (96.55%) | Memory: 52.2 MB (89.66%) - LeetSync

---
+++ 664-strange-printer/strange-printer.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var strangePrinter = function (s) {
+    const n = s.length;
+    const dp = Array.from({ length: n }, () => Array(n).fill(0));
+
+    for (let i = 0; i < n; i++) {
+        dp[i][i] = 1;
+    }
+
+    for (let length = 2; length <= n; length++) {
+        for (let i = 0; i <= n - length; i++) {
+            const j = i + length - 1;
+            dp[i][j] = dp[i][j - 1] + 1;
+            for (let k = i; k < j; k++) {
+                if (s[k] === s[j]) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][k] + (k + 1 <= j - 1 ? dp[k + 1][j - 1] : 0));
+                }
+            }
+        }
+    }
+
+    return dp[0][n - 1];
+};
