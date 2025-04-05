3c5abbec3acd66164a035c7774151b9b458b90dc
Ergys Perdeda <ergysperdeda97@gmail.com>
2025-04-05 12:37:27 +0200

    Time: 15 ms (49.72%) | Memory: 17.9 MB (41.01%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
@@ -1,13 +1,10 @@
-class Solution(object):
-    def subsetXORSum(self, nums):
-        sum = 0
-        subsets = list(chain(*map(lambda x: combinations(nums, x), range(0, len(nums)+1))))
-
-        for subset in subsets:
-            xorTotal = 0
-            for num in subset:
-                xorTotal ^= num
-            sum += xorTotal
-
-        return sum
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        def subsetXOR(index: int, currentXOR: int) -> int:
+            if index == len(nums):
+                return currentXOR
+            include = subsetXOR(index + 1, currentXOR ^ nums[index])
+            exclude = subsetXOR(index + 1, currentXOR)
+            return include + exclude
         
+        return subsetXOR(0, 0)
